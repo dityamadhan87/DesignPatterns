87cc4ec9a075941e9579b4a39305e2813acbc501
Raditya Rahmadhani <edogawaradit@gmail.com>
2025-06-29 08:40:07 +0700

    Implement State Pattern with GumballMachine and associated states

---
+++ src/statePattern/gumballState/HasQuarterState.java
@@ -0,0 +1,39 @@
+package statePattern.gumballState;
+
+public class HasQuarterState implements State{
+    GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned the crank");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+
+    @Override
+    public void refill() {}
+
+    @Override
+    public String toString() {
+        return "waiting for turn of crank";
+    }
+}
